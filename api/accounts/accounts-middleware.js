ed8d4923095a2de32be34a5b707794bc57315b28
Gözde G <gzde.gurler@gmail.com>
2023-02-06 19:30:20 +0300

    update

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,54 @@
+const db = require("../../data/db-config");
+const Account = require("./accounts-model");
+
 exports.checkAccountPayload = (req, res, next) => {
-  next()
-}
+  const { name, budget } = req.body;
+  // const name = req.body.name
+  // const budget = req.body.budget
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  next()
-}
+  if (name === undefined || budget === undefined) {
+    next({ status: 400, message: "name and budget are required" });
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    next({ status: 400, message: "name of account must be between 3 and 100" });
+  } else if (typeof budget !== "number" || isNaN(budget)) {
+    next({ status: 400, message: "budget of account must be a number" });
+  } else if (budget < 0 || budget > 1000000) {
+    next({
+      status: 400,
+      message: "budget of account is too large or too small",
+    });
+  } else {
+    next();
+  }
+};
 
-exports.checkAccountId = (req, res, next) => {
-  next()
-}
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const presentAccount = await db("accounts")
+      .where("name", req.body.name.trim())
+      .first();
+
+    if (presentAccount) {
+      next({ status: 400, message: "that name is taken" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id);
+
+    if (!account) {
+      next({ status: 404, message: "that name is taken" });
+    } else {
+      req.account = account;
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
